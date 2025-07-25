6ae7c949a7378fa202ff958637139f1ba7d9639f
Samwel Gachiri <107980554+samwel-gachiri@users.noreply.github.com>
2025-07-25 12:03:41 +0300

    refactor: clean up ChatController formatting and comment out unused method

--- src/main/java/com/alibou/websocket/chat/ChatController.java
+++ src/main/java/com/alibou/websocket/chat/ChatController.java
@@ -12,19 +12,17 @@ public class ChatController {
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(
-            @Payload ChatMessage chatMessage
-    ) {
+            @Payload ChatMessage chatMessage) {
         return chatMessage;
     }
 
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public ChatMessage addUser(
-            @Payload ChatMessage chatMessage,
-            SimpMessageHeaderAccessor headerAccessor
-    ) {
-        // Add username in web socket session
-        // headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        return chatMessage;
-    }
+    // @MessageMapping("/chat.addUser")
+    // @SendTo("/topic/public")
+    // public ChatMessage addUser(
+    //         @Payload ChatMessage chatMessage,
+    //         SimpMessageHeaderAccessor headerAccessor) {
+    //     // Add username in web socket session
+    //     headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+    //     return chatMessage;
+    // }
 }
