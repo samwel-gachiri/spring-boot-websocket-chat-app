e7970f41a502054b8e65c9d3f3f30d4cbfd33b08
Samwel Gachiri <107980554+samwel-gachiri@users.noreply.github.com>
2025-07-25 23:57:56 +0300

    Add user handling functionality to ChatController
    Add a method to handle user addition in chat sessions by storing the username in the WebSocket session attributes. This enables user tracking within the chat application.

--- src/main/java/com/alibou/websocket/chat/ChatController.java
+++ src/main/java/com/alibou/websocket/chat/ChatController.java
@@ -16,4 +16,13 @@ public class ChatController {
         return chatMessage;
     }
 
+    // @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(
+            @Payload ChatMessage chatMessage,
+            SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
 }
